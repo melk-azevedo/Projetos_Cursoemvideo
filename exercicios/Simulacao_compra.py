9622bc9e3af5d59d9f6b7564953382935f41dea8
Melk Azevedo <melk.azevedo@outlook.com>
2022-06-18 19:21:42 -0300

    correção na escrita

--- exercicios/Simulacao_compra.py
+++ exercicios/Simulacao_compra.py
@@ -7,7 +7,6 @@ cartao = compras - (compras * 5 / 100)
 juros = compras / 0.80
 
 
-
 print('''\nFORMAS DE PAGAMENTO
 [ 1 ] à vista no dinheiro
 [ 2 ] à vista no cartão
@@ -18,13 +17,15 @@ opcao = int(input('Digite a opção: '))
 
 
 if opcao == 1:
-	print(f'\nSua compra teve um desconto de 10% e o total é  R${avista:.2f}.')
+    print(f'\nSua compra teve um desconto de 10% e o total é  R${avista:.2f}.')
 elif opcao == 2:
-	print('Sua compra teve um desconto de 5% e o total é R${cartao:.2f} ')
+    print(f'Sua compra teve um desconto de 5% e o total é R${cartao:.2f}')
 elif opcao == 3:
-	print(f'sua compra será parcelada em 2x de R${compras/2:.2f} ')
+    print(f'sua compra será parcelada em 2x de R${compras/2:.2f} ')
 elif opcao == 4:
-	parcela = int(input('Quantas parcelas: '))
-	print(f'''Sua compra será parcelada em {parcela}x de {juros / parcela:.2f} COM JUROS.\nSua compra de R${compras:.2f} vai custar R${juros:.2f} no final.''')
+    parcela = int(input('Quantas parcelas: '))
+    print(
+        f'''Sua compra será parcelada em {parcela}x de {juros / parcela:.2f} COM JUROS.
+Sua compra de R${compras:.2f} vai custar R${juros:.2f} no final.''')
 else:
     print('\nOpção invalida, por favor tente outra vez!')
