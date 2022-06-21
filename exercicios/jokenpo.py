f7a4752f8f2d57701834b153ab4c3faa1029fb4e
Melk Azevedo <melk.azevedo@outlook.com>
2022-06-20 22:00:37 -0300

    Implementação do placar.
    Implantei um sistema de pontos. Onde quem completar 3 primeiro é o vencedor.

--- exercicios/jokenpo.py
+++ exercicios/jokenpo.py
@@ -7,41 +7,60 @@ print('-=--' * 5)
 
 print('')
 
-computador = randint(1, 3)
-if computador == 1:
-    computador = 'PEDRA'
-elif computador == 2:
-    computador = 'PAPEL'
-elif computador == 3:
-    computador = 'TESOURA'
+pont_c = 0
+pont_j = 0
+rodada = 0
 
-jogador = int(input('''Faça a sua escolha:
-[ 1 ] PEDRA
-[ 2 ] PAPEL
-[ 3 ] TESOURA\n'''))
-if jogador == 1:
-    jogador = 'PEDRA'
-elif jogador == 2:
-    jogador = 'PAPEL'
-elif jogador == 3:
-    jogador = 'TESOURA'
+while rodada < 3:
+    computador = randint(1, 3)
+    if computador == 1:
+        computador = 'PEDRA'
+    elif computador == 2:
+        computador = 'PAPEL'
+    elif computador == 3:
+        computador = 'TESOURA'
 
-print('JO')
-sleep(1)
-print('KEN')
-sleep(1)
-print('PO!!')
+    jogador = int(input('''Faça a sua escolha:
+    [ 1 ] PEDRA
+    [ 2 ] PAPEL
+    [ 3 ] TESOURA\n'''))
+    if jogador == 1:
+        jogador = 'PEDRA'
+    elif jogador == 2:
+        jogador = 'PAPEL'
+    elif jogador == 3:
+        jogador = 'TESOURA'
+    else:
+        print('Jogada inválida')
 
-print(f'\nComputador escolheu {computador} e Jogador escolheu {jogador}.\n')
+    print('JO')
+    sleep(1)
+    print('KEN')
+    sleep(1)
+    print('PO!!')
 
+    print(
+        f'\nComputador escolheu {computador} e Jogador escolheu {jogador}.\n')
+    if computador == jogador:
+        print('Poxa deu empate, que tal outra vez ?\n')
+    elif computador == 'PEDRA' and jogador == 'TESOURA':
+        print('Computador venceu a rodada.\n')
+        rodada += 1
+        pont_c += 1
+    elif computador == 'PAPEL' and jogador == 'PEDRA':
+        print('Computador venceu a rodada.\n')
+        pont_c += 1
+    elif computador == 'TESOURA' and jogador == 'PAPEL':
+        print('Computador venceu a rodada.\n')
+        rodada += 1
+        pont_c += 1
+    else:
+        print('Jogador venceu a rodada.\n')
+        rodada += 1
+        pont_j += 1
 
-if computador == jogador:
-    print('Poxa deu empate, que tal outra vez ?')
-elif computador == 'PEDRA' and jogador == 'TESOURA':
-    print('Computador venceu..')
-elif computador == 'PAPEL' and jogador == 'PEDRA':
-    print('Computador venceu..')
-elif computador == 'TESOURA' and jogador == 'PAPEL':
-    print('Computador venceu..')
+print(f'O resultado foi computador {pont_c} vs {pont_j} jogador.\n')
+if pont_c > pont_j:
+    print('Computador ganhou!')
 else:
-    print('Parabéns jogador venceu!')
+    print('Parabéns jogador ganhou!')
