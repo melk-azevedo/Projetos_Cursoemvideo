10a99505c8ddc978ddfabb58ed1262942c4486e3
Melk Azevedo <melk.azevedo@outlook.com>
2022-06-22 21:08:08 -0300

    correção
    Corrigindo endentação

--- exercicios/menu_opcoes.py
+++ exercicios/menu_opcoes.py
@@ -1,42 +1,42 @@
-from time import sleep 
+from time import sleep
 
 
 opcao = 0
 n1 = int(input('Primeiro valor: '))
 n2 = int(input('Segundo valor: '))
 while opcao != 5:
-	print('''
-	[ 1 ] somar
-	[ 2 ] multiplicar
-	[ 3 ] maior
-	[ 4 ] novos números
-	[ 5 ] sair do programa
-	''')
-	opcao = int(input('>>>>>>>Escolha uma opção: '))
-	if opcao == 1:
-		somar = n1 + n2
-		print(f'A soma de {n1} + {n2} é {somar}.')
-	elif opcao == 2:
-		multi = n1 * n2
-		print(f'A multiplicação de  {n1} × {n2} é {multi}')
-	elif opcao == 3:
-		if n1 > n2:
-			print(f'Entre {n1} e {n2} o maior é {n1}')
-		elif n1 < n2:
-			print(f'Entre {n1} e {n2} o maior é {n2}')
-		else:
-			print('Nenhum número é maior os dois são iguais.')
-	elif opcao == 4:
-		print('Digite os numeros novamente:')
-		n1 = int(input('Primeiro valor: '))
-		n2 = int(input('Segundo valor: '))
-	else:
-		if opcao != 5:
-			print('Opção inválida!')
-	print('==-'*10)
-	sleep(1)
+    print('''
+[ 1 ] somar
+[ 2 ] multiplicar
+[ 3 ] maior
+[ 4 ] novos números
+[ 5 ] sair do programa
+''')
+    opcao = int(input('>>>>>>>Escolha uma opção: '))
+    if opcao == 1:
+        somar = n1 + n2
+        print(f'A soma de {n1} + {n2} é {somar}.')
+    elif opcao == 2:
+        multi = n1 * n2
+        print(f'A multiplicação de  {n1} × {n2} é {multi}')
+    elif opcao == 3:
+        if n1 > n2:
+            print(f'Entre {n1} e {n2} o maior é {n1}')
+        elif n1 < n2:
+            print(f'Entre {n1} e {n2} o maior é {n2}')
+        else:
+            print('Nenhum número é maior os dois são iguais.')
+    elif opcao == 4:
+        print('Digite os numeros novamente:')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+    else:
+        if opcao != 5:
+            print('Opção inválida!')
+    print('==-'*10)
+    sleep(1)
 else:
-	print('Finalizando...')
+    print('Finalizando...')
 
 sleep(2)
 print('Fim do programa volte sempre!')
