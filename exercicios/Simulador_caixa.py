15e4c4d320702ac1540c5babe2975bccb6532602
Melk Azevedo <107591830+melk-azevedo@users.noreply.github.com>
2022-08-26 13:30:28 -0300

    Simulador de caixa eletrônico
    Um simulador simples de caixa eletrônico, coloquei notas de "1" apenas para facilitar o programa.

---
+++ exercicios/Simulador_caixa.py
@@ -0,0 +1,26 @@
+from time import sleep
+
+
+valor = int(input('Qual o valor que você quer sacar R$'))
+total = valor
+ced = 50
+totalced = 0 
+while True:
+	if total >= ced:
+		total -= ced
+		totalced += 1
+	else:
+		if totalced > 0:
+			print('Contando as notas.')
+			sleep(2)
+			print(f'Foram {totalced} cédulas de R${ced}')
+		if ced == 50:
+			ced = 20
+		elif ced == 20:
+			ced = 10
+		elif ced == 10:
+			ced = 1
+		totalced = 0
+		if total == 0: 
+			break
+print('Volte sempre!')
